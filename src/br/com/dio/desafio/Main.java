04fcb25cee478d4d06cd2ca8084406194e6b0a87
Denis Munerolli <denis.wilsom.munerolli@gmail.com>
2024-06-07 20:28:16 -0300

    init
    Mentoria e Curso

---
+++ src/br/com/dio/desafio/Main.java
@@ -0,0 +1,36 @@
+package br.com.dio.desafio;
+
+import java.time.LocalDate;
+
+import javax.xml.crypto.Data;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		
+		Curso curso1 = new Curso();
+		curso1.setTitulo("Curso java");
+		curso1.setDescricao("Descriçao do curso");
+		curso1.setCargaHoraria(8);
+		
+		Curso curso2 = new Curso();
+		curso2.setTitulo("Curso Js");
+		curso2.setDescricao("Descriçao do curso de Js");
+		curso2.setCargaHoraria(4);
+		
+		
+		mentoria mentoria1 = new mentoria();
+		mentoria1.setTitulo("Mentoria java");
+		mentoria1.setDescricao("Descriçao do curso de java");
+		mentoria1.setData(LocalDate.now());
+		
+		System.out.println(curso1);
+		System.out.println(curso2);
+		System.out.println(mentoria1);
+		
+		
+		
+	}
+
+}
